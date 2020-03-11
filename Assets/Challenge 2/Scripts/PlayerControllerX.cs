9d6f43028380d537c785334e9474ebb580dd210a
Ben <benmorris930@gmail.com>
2020-03-11 10:49:41 -0700

    Challenge2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float lastPress = -0.5f;
+    public float dogsPerSecond = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastPress > dogsPerSecond)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastPress = Time.time;
+        }
+    }
+}
